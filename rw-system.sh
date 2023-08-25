1ce8c6a0a555e265142f8ff29c88c544eb1d398a
Svirusx <45526199+Svirusx@users.noreply.github.com>
2023-08-25 14:26:46 +0200

    Fix Samsung Fingerprint detection

--- rw-system.sh
+++ rw-system.sh
@@ -372,7 +372,7 @@ if grep vendor.huawei.hardware.biometrics.fingerprint /vendor/manifest.xml; then
 fi
 
 foundFingerprint=false
-for manifest in /vendor/manifest.xml /vendor/etc/vintf /odm/etc/vintf;do
+for manifest in /vendor/manifest.xml /vendor/etc/vintf/manifest.xml /odm/etc/vintf;do
 	if grep -q \
 		-e android.hardware.biometrics.fingerprint \
 		-e vendor.oppo.hardware.biometrics.fingerprint \
