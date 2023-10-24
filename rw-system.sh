9c78b79f730f5c98542be12676f2347f2ebec93a
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-10-24 17:14:24 +0700

    Fix RIL for Galaxy J2 Prime

--- rw-system.sh
+++ rw-system.sh
@@ -541,6 +541,11 @@ if getprop ro.vendor.build.fingerprint | grep -iq -e iaomi/cactus -e iaomi/cereu
     setprop debug.stagefright.omx_default_rank 0
 fi
 
+if getprop ro.boot.bootloader | grep -q G532; then
+	setprop persist.sys.phh.sdk_override /vendor/bin/hw/rild=27
+    setprop persist.sys.phh.radio.use_old_mnc_format true
+fi
+
 mount -o bind /system/phh/empty /vendor/lib/libpdx_default_transport.so
 mount -o bind /system/phh/empty /vendor/lib64/libpdx_default_transport.so
 
