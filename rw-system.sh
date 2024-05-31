29745a6ba6251f8791469795607cda7b1887d82d
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-05-31 14:43:56 +0500

    Update rw-system.sh

--- rw-system.sh
+++ rw-system.sh
@@ -250,7 +250,7 @@ changeKeylayout() {
         changed=true
     fi
 
-    if getprop ro.vendor.build.fingerprint |grep -q -e samsung/a10sxx;then
+    if getprop ro.vendor.build.fingerprint |grep -iq -e samsung/a10sxx;then
         mkdir -p /data/vendor/mcRegistry
         chown system /data/vendor/mcRegistry
         chmod 755 /data/vendor/mc/Registry
