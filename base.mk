bf7b9ab53d177cefe8d2bfe78ef45b8f585f6759
jaimbo <jay@jayharkess.uk>
2026-05-11 21:35:30 +0100

    Add file to be copied to system

--- base.mk
+++ base.mk
@@ -136,7 +136,8 @@ PRODUCT_COPY_FILES += \
     device/phh/treble/files/moto-liber-gpio-keys.kl.raw:system/phh/moto-liber-gpio-keys.kl \
     device/phh/treble/files/tecno-touchpanel.kl.raw:system/phh/tecno-touchpanel.kl \
     device/phh/treble/files/samsung-sec_e-pen.idc:system/usr/idc/sec_e-pen.idc \
-    device/phh/treble/files/rosemary-excluded-input-devices.xml:system/phh/rosemary-excluded-input-devices.xml
+    device/phh/treble/files/rosemary-excluded-input-devices.xml:system/phh/rosemary-excluded-input-devices.xml \
+    device/phh/treble/files/lenovo-tb710fu-hall_irq.kl.raw:system/phh/lenovo-tb710fu-hall_irq.kl \
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
